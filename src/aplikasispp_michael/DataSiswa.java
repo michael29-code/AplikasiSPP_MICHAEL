9604230bd1e975daabd9c23dd2c79a7b692090c7
Michael <alfonsusmichael2003@gmail.com>
2022-02-15 12:56:24 +0700

    update
    add select and update data

--- src/aplikasispp_michael/DataSiswa.java
+++ src/aplikasispp_michael/DataSiswa.java
@@ -11,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -82,7 +83,7 @@ public class DataSiswa extends javax.swing.JFrame {
             rs = pst.executeQuery();
             
             while(rs.next()){
-                combobox_kelas.addItem(rs.getString("nama_kelas"));
+                combobox_kelas.addItem(rs.getString("id_kelas"));
             }
         } catch (SQLException ex) {
             System.out.println("Gagal Menampilakn Combobox Kelas");
@@ -118,12 +119,12 @@ public class DataSiswa extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txt_nisn = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        btnTambah = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        btnUbah = new javax.swing.JButton();
+        btnHapus = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         txt_nama_pesdik = new javax.swing.JTextField();
         txt_alamat = new javax.swing.JTextField();
@@ -151,10 +152,10 @@ public class DataSiswa extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("Tambah");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnTambah.setText("Tambah");
+        btnTambah.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnTambahActionPerformed(evt);
             }
         });
 
@@ -171,16 +172,21 @@ public class DataSiswa extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
 
-        jButton2.setText("Edit");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnUbah.setText("Edit");
+        btnUbah.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnUbahActionPerformed(evt);
             }
         });
 
-        jButton3.setText("Hapus");
+        btnHapus.setText("Hapus");
 
         jLabel4.setText("Nama Pesdik");
 
@@ -258,9 +264,9 @@ public class DataSiswa extends javax.swing.JFrame {
                                     .addComponent(combo_box_id_spp, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                         .addGap(57, 57, 57)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(btnHapus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnTambah, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnUbah, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(49, 49, 49))))
         );
         layout.setVerticalGroup(
@@ -272,15 +278,15 @@ public class DataSiswa extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(txt_nisn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                    .addComponent(btnTambah))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jButton2)
+                    .addComponent(btnUbah)
                     .addComponent(combobox_kelas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton3)
+                    .addComponent(btnHapus)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel4)
@@ -317,13 +323,40 @@ public class DataSiswa extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txt_nisnActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnTambahActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTambahActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnTambahActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void btnUbahActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUbahActionPerformed
+       
+        try {
+            int row = jTable1.getSelectedRow();
+            String diklik = (jTable1.getModel().getValueAt(row, 0).toString());
+            String ubah = "UPDATE siswa set id_kelas = ?, nama_pesdik = ? , alamat = ?, telepon = ?,hp = ?,id_spp = ? WHERE nisn = ?";
+            pst = conn.prepareStatement(ubah);
+            
+            
+            String isiKelas = (String)combobox_kelas.getSelectedItem();
+            pst.setString(1, isiKelas);
+            
+            pst.setString(2, txt_nama_pesdik.getText());
+            pst.setString(3, txt_alamat.getText());
+            pst.setString(4, txt_telepon.getText());
+            pst.setString(5, txt_hp.getText());
+            
+            String isispp =(String)combo_box_id_spp.getSelectedItem();
+            pst.setString(6, isispp);
+            pst.setString(7, diklik);
+            
+            
+            pst.execute();
+            JOptionPane.showMessageDialog(null, "Data Berhasil Di update");
+            tampilkan_datasiswa();
+//          kosongkan();
+        } catch (SQLException ex) {
+            Logger.getLogger(DataSiswa.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btnUbahActionPerformed
 
     private void txt_nama_pesdikActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_nama_pesdikActionPerformed
         // TODO add your handling code here:
@@ -340,6 +373,43 @@ public class DataSiswa extends javax.swing.JFrame {
     private void txt_hpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_hpActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txt_hpActionPerformed
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        
+      
+        try {
+            int row = jTable1.getSelectedRow();
+            String diklik = (jTable1.getModel().getValueAt(row, 0).toString());
+            String keklik = "SELECT * FROM siswa where nisn='"+diklik+"'";
+            pst = conn.prepareStatement(keklik);
+            rs = pst.executeQuery();
+            
+            if ((rs.next())) {
+                String set1 = rs.getString("nisn");
+                txt_nisn.setText(set1);
+                
+                String isikelas = rs.getString("id_kelas");
+                combobox_kelas.setSelectedItem(isikelas);
+                
+                String set2 = rs.getString("nama_pesdik");
+                txt_nama_pesdik.setText(set2);
+                
+                String set3 = rs.getString("alamat");
+                txt_alamat.setText(set3);
+                
+                String set4 = rs.getString("telepon");
+                txt_telepon.setText(set4);
+                
+                String set5 = rs.getString("hp");
+                txt_hp.setText(set5);
+                
+                String isispp = rs.getString("id_spp");
+                combo_box_id_spp.setSelectedItem(isispp);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DataSiswa.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jTable1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -377,11 +447,11 @@ public class DataSiswa extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnHapus;
+    private javax.swing.JButton btnTambah;
+    private javax.swing.JButton btnUbah;
     private javax.swing.JComboBox<String> combo_box_id_spp;
     private javax.swing.JComboBox<String> combobox_kelas;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
